517f8a81aacafb0a0ce667b5f2653d2ae185ebf0
MaanasaPriyaa <maanasa@mit.edu>
2017-05-02 20:38:34 -0400

    Unified serverless framework for all social media lambdas

--- README.md
+++ README.md
@@ -1,6 +1,67 @@
-cognicity-reports-lambda
+# cognicity-reports-lambda
 ========================
-Data connectors for CogniCity using AWS Lambda functions
+1. Data connectors for CogniCity using AWS Lambda functions
+2. Deploys serverless AWS Lambda webhook functions to listen to messages and send report link on Facebook and Telegram 3. Deploys serverless AWS Lambda webhook functions to send confirmation message and report link on submission of a report via Facebook Messenger, Telegram, and Twitter
 
 ## Current connectors
 1. Qlue
+2. Facebook Messenger Bot
+3. Telegram Bot
+4. Twitter Confirmation Reply Bot
+
+### Install
+`npm install`
+
+### For Facebook Messenger Bot
+* Create Facebook app and page and get `APPSECRET` and `PAGEACCESSTOKEN` as explained [here](https://developers.facebook.com/docs/messenger-platform/guides/setup). Hold off on completing Step 2 (Set up webhooks) in the guide.
+* Add code for webhooks and add it to the functions in serverless.yml file as explained [here](https://serverless.com/blog/building-a-facebook-messenger-chatbot-with-serverless/)
+* Add the lambda function to listen to relevant SNS topics and add it to the functions in serverless.yml file
+* Set up the config files as explained in the `Configuration` section. `VALIDATIONTOKEN` can be set to any arbitrary string.
+* Now, deploy your serverless lambdas as mentioned in the `Run` section
+* On successful deployment, you'll get a secure URL for the webhook's GET method. Now complete Step 2 in the [Quick Start Guide](https://developers.facebook.com/docs/messenger-platform/guides/setup). Set this secure URL as 'Callback URL' and `VALIDATIONTOKEN` in the 'Verify Token' fields. Select 'messages' and 'messaging_postbacks' to enable two-way communication. Verifying and saving this enables the webhooks for the app.
+* Subscribe your app to your page as explained [here](https://developers.facebook.com/docs/messenger-platform/guides/setup#subscribe_app)
+* Send a text to your Facebook messenger bot to test if it is up and running!
+* Read `Misc Notes` section to assist in configuration
+
+### For Telegram Bot
+* Create Telegram bot as explained [here](https://core.telegram.org/bots#creating-a-new-bot) and get `BOT_TOKEN` [here](https://core.telegram.org/bots#generating-an-authorization-token). Configure its About, Description, Commands, Profile picture but hold off on setting up Webhooks until you have configured the webhook.
+* Add code for webhooks in handler.js and add it to the functions in serverless.yml file as explained [here](https://medium.com/zingle/creating-a-server-less-telegram-bot-with-aws-lambda-and-aws-api-gateway-36406471b2ca)
+* Add the lambda function to listen to relevant SNS topics and add it to the functions in serverless.yml file
+* Set up the config files as explained in the `Configuration` section.
+* Now, deploy your serverless lambdas as mentioned in the `Run` section
+* On successful deployment, you'll get a secure URL for the webhook. Now expose it via API Gateway POST method and deploy it. Set up this API's URL as the webhook as described in this [Guide](https://core.telegram.org/bots/api#setwebhook).
+* Send a text to your Telegram bot to test if it is up and running!
+* Read `Misc Notes` section to assist in configuration
+
+### For Twitter App
+* Create Twitter app as described [here] (http://docs.inboundnow.com/guide/create-twitter-application/) and collect the keys & secret tokens.
+* Set up the config files as explained in the `Configuration` section.
+* Read `Misc Notes` section to assist in configuration
+
+### Run
+`serverless deploy`
+
+### Configuration
+Save a copy of sample.env as .env in local directory with appropriate credentials
+
+* `BOT_TOKEN`: Access token created on creating a Telegram bot
+* `VALIDATIONTOKEN`: Give the same token on Facebook dev portal to validate webhooks
+* `APPSECRET`: AppSecret key got on creating facebook app
+* `PAGEACCESSTOKEN`: Access token for the page to which the app has subscribed
+* `TWITTER_CONSUMER_KEY`: Taken from twitter dev admin interface after creating an app
+* `TWITTER_CONSUMER_SECRET`: Taken from twitter dev admin interface after creating an app
+* `TWITTER_ACCESS_TOKEN_KEY`: Taken from twitter dev admin interface after creating an app
+* `TWITTER_ACCESS_TOKEN_SECRET`: Taken from twitter dev admin interface after creating an app
+* `DEFAULT_LANG`: Current default language is English. You can add more languages here and parameterize replies for each language.
+* `CARD_PATH`: Front end's cards URL
+* `MAPSERVER`: Front end's map URL
+* `X_API_KEY`: API Key needed to make calls to the deployed server (Set it to "" during local testing)
+* `PG_CON`: Connection string for the Postgres database
+* `SERVER`: Cognicity server URL to fetch unique cardIds
+* `BOTNAME`: Bot/Platform name to be sent in the Greeting text.
+
+#### Misc Notes
+- AWS credentials are stored in bash_profile
+- Grasp "username" is userID/senderID from source networks to allow replies in conversation
+- Errors are logged to console, but not returned to user currently
+- If you want to test with your local Cognicity server instance, set up secure tunnels to localhost using ngrok and use that URL in the .env file. Install 'npm install -g ngrok'. After initializing the server, run 'ngrok http <PORT_NUMBER'. Use the https URL generated and set it in the 'SERVER' section of the env file. This allows the Lambda to interact with the server to fetch card OTL.
---
+++ handler.js
@@ -0,0 +1,328 @@
+'use strict';
+
+const request = require('request');
+require('dotenv').config({
+  silent: true
+});
+
+// Twitter Client to send reply tweets
+var Twitter = require('twitter');
+var twitterClient = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+// GRASP card
+const options = {
+  host: process.env.SERVER,
+  path: '/cards',
+  method: 'POST',
+  port: 80,
+  headers: {
+    'x-api-key': process.env.X_API_KEY,
+    'Content-Type': 'application/json'
+  }
+}
+
+// GRASP operating regions
+const instance_regions = {
+  chn: 'chennai',
+  jbd: 'jakarta',
+  sby: 'surabaya',
+  bdg: 'bandung'
+}
+
+// Replies to user
+const replies = {
+  'en': 'Hi! Report using this link, thanks.',
+  'id': 'Hi! Laporkan menggunakan link ini. Terima kasih.'
+}
+
+// Confirmation message to user
+const confirmations = {
+  'en': "Hi! Thanks for your report. I've put it on the map.",
+  'id': 'Hi! Terima kasih atas laporan Anda. Aku sudah menaruhnya di peta.'
+}
+
+/*
+ * Construct the initial message to be sent to the user
+ */
+function getInitialMessageText(language, cardId, disasterType) {
+  return replies[language] + "\n" + process.env.CARD_PATH + "/" + disasterType + "/" + cardId + "/report";
+}
+
+/*
+ * Construct the confirmation message to be sent to the user
+ */
+function getConfirmationMessageText(language, implementationArea, reportId) {
+  return confirmations[language] + "\n" + process.env.MAPSERVER + instance_regions[implementationArea] + '/' + reportId;
+}
+
+/*
+ * Get one time card link from the server
+ */
+function getCardLink(username, network, language, callback) {
+  var card_request = {
+    "username": username,
+    "network": network,
+    "language": language
+  }
+
+  // Get a card from Cognicity server
+  request({
+    url: options.host + options.path,
+    method: options.method,
+    headers: options.headers,
+    port: options.port,
+    json: true,
+    body: card_request
+  }, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      callback(null, body.cardId); //Return cardId on success
+    } else {
+      var err = 'Error getting card: ' + JSON.stringify(error) + JSON.stringify(response);
+      callback(err, null); // Return error
+    }
+  });
+}
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
+ *
+ */
+function sendFacebookMessage(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: process.env.PAGEACCESSTOKEN
+    },
+    method: 'POST',
+    json: messageData
+
+  }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+        console.log("Successfully called Send API for recipient %s",
+          recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });
+}
+
+/*
+ * Calls the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
+ *
+ */
+function sendTelegramMessage(messageData, senderID) {
+  request({
+    uri: 'https://api.telegram.org/bot' + process.env.BOT_TOKEN + '/sendmessage?text=' + messageData + '&chat_id=' + senderID,
+    method: 'POST'
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var responseBody = JSON.parse(body);
+      var result = responseBody.result;
+      var recipientId = result.chat.id;
+      var messageId = result.message_id;
+
+      if (messageId) {
+        console.log('Successfully sent message with id %s to recipient %s',
+          messageId, recipientId);
+      } else {
+      console.log('Successfully called Send API for recipient %s',
+        recipientId);
+      }
+    } else {
+      console.error('Failed calling Send API', response.statusCode, body.error_code, body.description);
+    }
+  });
+}
+
+/*
+ * Makes POST call to post a Twitter status update @ the user's handle
+ */
+function sendTweet(messageText, userName) {
+  twitterClient.post('statuses/update', {status: messageText})
+    .then(function (tweet) {
+      console.log('Tweet sent: ' + tweet + 'to the user: ' + userName);
+    })
+    .catch(function (error) {
+      console.error('Sending report link tweet failed', error);
+    })
+}
+
+// Webhook handler - This is the method called by Facebook when you verify webhooks for the app
+module.exports.facebookWebhook = (event, context, callback) => {
+  console.log(JSON.stringify(event.body));
+  if (event.method === 'GET') {
+    // Facebook app verification
+    if (event.query['hub.verify_token'] === process.env.VALIDATIONTOKEN && event.query['hub.challenge']) {
+      return callback(null, parseInt(event.query['hub.challenge']));
+    } else {
+      return callback('Invalid token');
+    }
+  }
+
+  if (event.method === 'POST') {
+    event.body.entry.map((entry) => {
+      entry.messaging.map((messagingItem) => {
+        if (messagingItem.message && messagingItem.message.text && //Code can be removed after updating Petabencana bot because we want to use only menu based communication
+          (messagingItem.message.text.toLowerCase().includes('banjir') ||
+            messagingItem.message.text.toLowerCase().includes('flood'))) {
+          // Form JSON request body
+          var language = process.env.DEFAULT_LANG;
+          if (messagingItem.message.text.toLowerCase().includes('flood')) {
+            language = 'en';
+          }
+
+          getCardLink (messagingItem.sender.id.toString(), "facebook", language, function(error, cardId) {
+            if(error) {
+              console.log(error);
+            } else {
+              var messageText = getInitialMessageText(language, cardId, 'flood');
+              const payload = {
+                recipient: {
+                  id: messagingItem.sender.id
+                },
+                message: {
+                  text: messageText
+                }
+              };
+              sendFacebookMessage(payload);
+            }
+          });
+        } else if (messagingItem.postback && messagingItem.postback.payload) {
+          if (messagingItem.postback.payload === "GET_STARTED_PAYLOAD") {
+            var payload = {
+              recipient: {
+                id: messagingItem.sender.id
+              },
+              message: {
+                attachment: {
+                  type: "template",
+                  payload: {
+                    template_type: "button",
+                    text: "Please select one of the below options to get one-time link for reporting",
+                    buttons: [{
+                        "type": "postback",
+                        "title": "Report flood",
+                        "payload": "flood"
+                      },
+                      {
+                        "type": "postback",
+                        "title": "Report grievances",
+                        "payload": "prep"
+                      }
+                    ]
+                  }
+                }
+              }
+            };
+            sendFacebookMessage(payload);
+          } else if (messagingItem.postback.payload === "flood" || messagingItem.postback.payload === "prep") {
+            var language = process.env.DEFAULT_LANG;
+            getCardLink (messagingItem.sender.id.toString(), "facebook", language, function(error, cardId) {
+              if(error) {
+                console.log(error);
+              } else {
+                var messageText = getInitialMessageText(language, cardId, messagingItem.postback.payload);
+                const payload = {
+                  recipient: {
+                    id: messagingItem.sender.id
+                  },
+                  message: {
+                    text: messageText
+                  }
+                };
+                sendFacebookMessage(payload);
+              }
+            });
+          }
+        }
+      });
+    });
+  }
+};
+
+module.exports.facebookReply = (event, context, callback) => {
+  //This module listens in to SNS Facebook topic and reads the message published
+  var message = JSON.parse(event.Records[0].Sns.Message);
+  console.log("Message received from SNS topic: " + message);
+
+  var messageText = getConfirmationMessageText(message.language, message.implementation_area, message.report_id);
+  const payload = {
+    recipient: {
+      id: message.username
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  //Call Send API to confirmation message with report link to the user
+  sendFacebookMessage(payload);
+};
+
+// Webhook handler - This is the method called by Telegram when user sends a message
+module.exports.telegramWebhook = (event, context, callback) => {
+  if (event.message && event.message.text && (event.message.text == "/flood" || event.message.text == "/prep")) {
+    // Form JSON request body
+    var chatID = event.message.chat.id;
+    console.log('Received flood report request via Telegram from: ' + chatID);
+
+    var language = process.env.DEFAULT_LANG;
+    getCardLink (chatID.toString(), "telegram", language, function(error, cardId) {
+      if(error) {
+        console.log(error);
+        callback(error, null);
+      } else {
+        var disasterType = (event.message.text.includes('flood') ? 'flood' : 'prep');
+        var messageText = getInitialMessageText(language, cardId, disasterType);
+        sendTelegramMessage(messageText, chatID);
+        var response = {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify({})
+        };
+        console.log('Sending success API Gateway response');
+        callback(null, response); // Send success code with empty json to avoid duplicate POST calls
+      }
+    });
+  }
+};
+
+module.exports.telegramReply = (event, context, callback) => {
+  //This module listens in to SNS Telegram topic and reads the message published
+  var message = JSON.parse(event.Records[0].Sns.Message);
+  console.log('Message received from SNS topic: ' + JSON.stringify(message));
+
+  //Construct the confirmation message to be sent to the user
+  var messageText = getConfirmationMessageText(message.language, message.implementation_area, message.report_id);
+
+  //Call Send API to confirmation message with report link to the user
+  sendTelegramMessage(messageText, message.username);
+};
+
+module.exports.twitterReply = (event, context, callback) => {
+  //This module listens in to SNS Twitter topic and reads the message published
+  var message = JSON.parse(event.Records[0].Sns.Message);
+  console.log('Message received from SNS topic: ' + message);
+
+  //Construct the confirmation message to be sent to the user
+  var messageText = getConfirmationMessageText(message.language, message.implementation_area, message.report_id);
+  var messageText = '@' + message.username + ' ' + messageText;
+
+  //Make a POST call to send a tweet to the user
+  sendTweet(messageText, message.username);
+};
