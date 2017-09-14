816855996262ff570c903ca6a9b7c726fcfdf6b5
abrahamq <abrahamquintero2323@gmail.com>
2017-09-13 22:30:41 -0400

    US and India deployment from one repo

--- decrypt.sh
+++ decrypt.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
 
-openssl aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in env.yml.enc -out testDecryption -d
+openssl aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in India.env.yml.enc -out India.env.yml -d
+openssl aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in US.env.yml.enc -out US.env.yml -d
--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 
 #will encrypt env.yml into env.yml.enc
 #environment variables $fb_telegram_param_iv and $fb_telegram_param_key must be set correctly
-openssl enc -aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in env.yml -out env.yml.enc 
+openssl enc -aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in India.env.yml -out India.env.yml.enc 
+openssl enc -aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in US.env.yml -out US.env.yml.enc 
